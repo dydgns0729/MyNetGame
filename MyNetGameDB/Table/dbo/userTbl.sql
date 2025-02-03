10baf48291b4d6a361893b8fae1308da9af5f93c
toneed <dydgns0729@gmail.com>
2025-02-03 09:48:38 +0900

    1. MyNetGameDB 프로젝트 생성 및 userTbl 테이블 생성 2. MyNetGame 프로젝트 생성 및 userTbl 읽고 쓰기 수정 삭제 구현 3. 유저 추가 기능 구현

---
+++ MyNetGameDB/Table/dbo/userTbl.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[userTbl]
+(
+	userId VARCHAR(20) NOT NULL PRIMARY KEY,		-- 유저 아이디 (이메일)
+	password VARCHAR(20) NOT NULL,					-- 비밀번호
+	mobile CHAR(11),							-- 유저 전화번호
+	level SMALLINT,								-- 유저 레벨
+	health INT NOT NULL,						-- 유저 체력
+	gold INT NOT NULL,							-- 유저 골드
+	mDate date									-- 유저 가입일
+)
+
